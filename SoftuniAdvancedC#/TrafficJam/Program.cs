73a68c47d6430465d2a946c39c913885593bca75
Teodor Ivanov <156572502+tedilito@users.noreply.github.com>
2024-09-10 00:57:46 +0300

    Add files via upload

---
+++ SoftuniAdvancedC#/TrafficJam/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int numOfCars = int.Parse(Console.ReadLine());
+        Queue<string> queue = new Queue<string>();
+        int count = 0;
+        string input;
+
+        while ((input = Console.ReadLine()) != "end")
+        {
+            if (input != "green")
+            {
+                queue.Enqueue(input);
+            }
+            else
+            {
+                int carsToPass = Math.Min(numOfCars, queue.Count);
+                for (int i = 0; i < carsToPass; i++)
+                {
+                    Console.WriteLine($"{queue.Dequeue()} passed!");
+                    count++;
+                }
+            }
+        }
+
+        Console.WriteLine($"{count} cars passed the crossroads.");
+    }
+}
